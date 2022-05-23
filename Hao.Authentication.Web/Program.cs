b7edd99e41c673552c93d7530af02e7f10bae7fc
sunhao <2080680175@@qq.com>
2022-05-23 17:55:20 +0800

    添加项目文件。

---
+++ Hao.Authentication.Web/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container.
+
+builder.Services.AddControllers();
+
+// Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#endregion
+
+var app = builder.Build();
+
+#region  Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+#endregion
+
+app.Run();
