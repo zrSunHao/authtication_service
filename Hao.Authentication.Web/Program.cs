23ec8304b2a9114424ee55e2afe0266a89eb4202
sunhao <2080680175@@qq.com>
2022-06-12 07:32:52 +0800

    add serilog!

--- Hao.Authentication.Web/Program.cs
+++ Hao.Authentication.Web/Program.cs
@@ -1,41 +1,66 @@
 using Hao.Authentication.Manager;
 using Hao.Authentication.Web.Extentions;
 using Hao.Authentication.Web.Middlewares;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            //.WriteTo.File("logs\\log_.txt", LogEventLevel.Warning, rollingInterval: RollingInterval.Day)
+            .CreateBootstrapLogger();
 
-#region Add services to the container.
-
-builder.Services.AddDbContext(builder.Configuration);
-builder.Services.AddMyCors(builder.Configuration);
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddRabbitMq(builder.Configuration);    
-builder.Services.DomainConfigureServices();
-builder.Services.AddControllers();
-builder.Services.AddSwagger();
-builder.Services.AddJwtAuthentication(builder.Configuration);
-
-#endregion
-
-var app = builder.Build();
-
-#region  Configure the HTTP request pipeline.
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web host");
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console());
+
+    #region Add services to the container.
+    builder.Services.AddDbContext(builder.Configuration);
+    builder.Services.AddMyCors(builder.Configuration);
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddRabbitMq(builder.Configuration);
+    builder.Services.DomainConfigureServices();
+    builder.Services.AddControllers();
+    builder.Services.AddSwagger();
+    builder.Services.AddJwtAuthentication(builder.Configuration);
+    #endregion
+
+    var app = builder.Build();
+    #region  Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging();
+    app.UseMiddleware<MonitorMiddleware>();
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseRouting();
+    app.UseMyCors();
+    app.UseAuthorization();
+    app.UseMiddleware<PrivilegeMiddleware>();
+    app.MapControllers();
+    #endregion
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseMiddleware<MonitorMiddleware>();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseRouting();
-app.UseMyCors();
-app.UseAuthorization();
-app.UseMiddleware<PrivilegeMiddleware>();
-app.MapControllers();
 
-#endregion
-
-app.Run();
