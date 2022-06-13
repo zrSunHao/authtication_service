d75c200e58e4e4d74131913d9eab9380d2a57edd
sunhao <2080680175@@qq.com>
2022-06-13 08:20:10 +0800

    publish completed!

--- Hao.Authentication.Web/Program.cs
+++ Hao.Authentication.Web/Program.cs
@@ -35,11 +35,13 @@ try
 
     var app = builder.Build();
     #region  Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseSwagger();
+    //    app.UseSwaggerUI();
+    //}
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseSerilogRequestLogging();
     
